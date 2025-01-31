ee620f54ff4c404e1f63aceded71e76b7f077466
chandiniratho <rathochandini@gmail.com>
2025-01-31 09:54:37 +0530

    Fix routing and styling for GitHub Pages deployment

--- src/App.tsx
+++ src/App.tsx
@@ -4,12 +4,13 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import AddEmployee from "./pages/AddEmployee";
 import EditEmployee from "./pages/EditEmployee";
-import './styles/styles.css';  // Adjust the path based on where you put it
+import './styles/styles.css';  // Make sure the path is correct
 
 const App: React.FC = () => {
   return (
     <EmployeeProvider>
-      <Router>
+      {/* Wrap Router with basename to handle GitHub Pages routing */}
+      <Router basename="/employee-management">
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/add" element={<AddEmployee />} />
